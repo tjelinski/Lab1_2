7097cfd4907c55a24ecb06cb507a599c7238ce34
t.jelinski <tjelinski@student.wsb-nlu.edu.pl>
2023-10-28 01:59:41 +0200

    crate class CreateGui

---
+++ src/CreateGui.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+// tjelinski@student.wsb-nlu.edu.pl
+public class CreateGui {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame();
+        frame.setSize(640,480);
+        frame.setVisible(true);
+    }
+}
